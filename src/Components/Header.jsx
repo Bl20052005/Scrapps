df7d2d97f5f97af6093d3f10b2ced86a6c1e0f83
Roy Park <roydp@uci.edu>
2024-07-02 20:28:25 -0700

    Changes made
    Adding exclamation on doctype.
    Removed placeholder texts.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,6 @@
 import styles from './Header.module.css'
 import MenuDirectories from './MenuDirectories'
 function Header(props){
-    console.log(props)
     return(
         <header>
             <MenuDirectories setCurrent={props.setCurrent}></MenuDirectories>
--- src/pages/Matchmake/Matchmake.jsx
+++ src/pages/Matchmake/Matchmake.jsx
@@ -2,7 +2,7 @@ import styles from './Matchmake.module.css'
 function Matchmake() {
     return (
         <div>
-            <p>Hello World</p>
+            
         </div>
     )
 }
